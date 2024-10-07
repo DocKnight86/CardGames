145cac61316a8aaaf228d92e3812f5cb0cc096e9
Dan <knight.daniel.r@gmail.com>
2024-10-07 12:48:40 -0400

    FinalStart

--- Services/BlackjackService.cs
+++ Services/BlackjackService.cs
@@ -2,34 +2,29 @@
 {
     public class BlackjackService
     {
-        private List<Card> deck;
-        private List<Card> playerHand;
-        private List<Card> dealerHand;
-        private bool dealerHandRevealed;
-
-        public BlackjackService()
-        {
-            InitializeGame();
-        }
+        private List<Card> _deck;
+        private List<Card> _playerHand;
+        private List<Card> _dealerHand;
+        private bool _dealerHandRevealed;
 
         public void InitializeGame()
         {
-            deck = GenerateDeck();
-            playerHand = new List<Card>();
-            dealerHand = new List<Card>();
-            dealerHandRevealed = false;
+            _deck = GenerateDeck();
+            _playerHand = new List<Card>();
+            _dealerHand = new List<Card>();
+            _dealerHandRevealed = false;
 
             // Deal initial cards
-            playerHand.Add(DrawCard());
-            playerHand.Add(DrawCard());
-            dealerHand.Add(DrawCard());  // Visible card
-            dealerHand.Add(DrawCard());  // Hidden card
+            _playerHand.Add(DrawCard());
+            _playerHand.Add(DrawCard());
+            _dealerHand.Add(DrawCard());  // Visible card
+            _dealerHand.Add(DrawCard());  // Hidden card
         }
 
         public void PlayerStands()
         {
-            int playerValue = CalculateHandValue(playerHand);
-            int dealerValue = CalculateHandValue(dealerHand);
+            int playerValue = CalculateHandValue(_playerHand);
+            int dealerValue = CalculateHandValue(_dealerHand);
 
             // Reveal dealer's second card
             RevealDealerHand();
@@ -37,8 +32,8 @@
             // Dealer will stand if their value is >= 17, otherwise they draw a card
             while (dealerValue < 17)
             {
-                dealerHand.Add(DrawCard());
-                dealerValue = CalculateHandValue(dealerHand);
+                _dealerHand.Add(DrawCard());
+                dealerValue = CalculateHandValue(_dealerHand);
             }
 
             if (dealerValue > 21)
@@ -60,59 +55,56 @@
 
         public void RevealDealerHand()
         {
-            dealerHandRevealed = true;
+            _dealerHandRevealed = true;
         }
 
         public List<Card> GetVisibleDealerHand()
         {
-            if (dealerHandRevealed)
+            if (_dealerHandRevealed)
             {
-                return dealerHand;
+                return _dealerHand;
             }
 
             // Only return the first card when the hand is not revealed
-            return new List<Card> { dealerHand[0] };
+            return new List<Card> { _dealerHand[0] };
         }
 
         private List<Card> GenerateDeck()
         {
-            var deck = new List<Card>();
-            var suits = new[] { "Hearts", "Diamonds", "Clubs", "Spades" };
-            var values = new[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            List<Card> deck = new List<Card>();
+            string[] suits = new[] { "Hearts", "Diamonds", "Clubs", "Spades" };
+            string[] values = new[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
 
-            foreach (var suit in suits)
+            foreach (string suit in suits)
             {
-                foreach (var value in values)
+                foreach (string value in values)
                 {
                     deck.Add(new Card(suit, value));
                 }
             }
 
-            var random = new Random();
+            Random random = new Random();
             return deck.OrderBy(x => random.Next()).ToList();
         }
 
         public Card DrawCard()
         {
-            var card = deck.First();
-            deck.Remove(card);
-
-            // Debug
-            Console.WriteLine(card);
+            Card card = _deck.First();
+            _deck.Remove(card);
 
             return card;
         }
 
-        public List<Card> GetPlayerHand() => playerHand;
+        public List<Card> GetPlayerHand() => _playerHand;
 
-        public List<Card> GetDealerHand() => dealerHand;
+        public List<Card> GetDealerHand() => _dealerHand;
 
         public int CalculateHandValue(List<Card> hand)
         {
             int totalValue = 0;
             int aceCount = 0;
 
-            foreach (var card in hand)
+            foreach (Card card in hand)
             {
                 if (int.TryParse(card.Value, out int cardValue))
                 {
@@ -139,7 +131,7 @@
             return totalValue;
         }
 
-        public virtual bool IsGameOver() => CalculateHandValue(playerHand) >= 21 || CalculateHandValue(dealerHand) >= 21;
+        public virtual bool IsGameOver() => CalculateHandValue(_playerHand) >= 21 || CalculateHandValue(_dealerHand) >= 21;
     }
 
     public class Card
